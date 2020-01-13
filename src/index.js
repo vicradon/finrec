f339c51f27ee78d1f84454cf61c04c251c455f33
Chukwujama Osinachi Victor <vicraph7@gmail.com>
2020-01-13 14:58:26 +0100

    Add PWA functionality

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App'
+import App from './components/App';
+import * as serviceWorker from './serviceWorker.js';
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+serviceWorker.register();
