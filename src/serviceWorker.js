845ca283795b168ff2217c73efe3a9300f5556f4
Chukwujama Osinachi Victor <vicraph7@gmail.com>
2020-01-14 12:25:07 +0100

    Little alteration to sw.js

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -58,7 +58,7 @@ function registerValidSW(swUrl, config) {
               // but the previous service worker will still serve the older
               // content until all client tabs are closed.
               console.log(
-                'New content is available and will be used when all ' +
+                'New content is available and will be used when all and I mean all' +
                   'tabs for this page are closed. See https://bit.ly/CRA-PWA.'
               );
 
