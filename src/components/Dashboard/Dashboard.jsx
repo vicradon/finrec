b93d5c146ceb53a6e5fcb110673949e06733a2d8
Chukwujama Osinachi Victor <vicraph7@gmail.com>
2020-05-03 08:34:42 +0100

    Add redux, remove contextAPI

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -4,12 +4,8 @@ import SummaryCards from "./SummaryCards";
 import IncomeExpenseCharts from "./IncomeExpenseCharts";
 import RecentTransactions from "./RecentTransactions";
 import TotalExpensePie from "./TotalExpensePie";
-import { StateContext, DispatchContext } from "../../state/Store";
 
 const Dashboard = () => {
-  const c = React.useContext(StateContext);
-  const d = React.useContext(DispatchContext);
-  console.log(c, d)
   return (
     <div>
       <Flex justify="space-between">
--- src/state/finance_state_reducer.js
+++ src/state/finance_state_reducer.js
@@ -3,11 +3,5 @@ export const initialFinancialState = {
 }
 
 export const financeStateReducer = (state, action) => {
-  const { payload } = action;
-  switch (action.type) {
-    case "ADD_ITEM":
-      return addNewItem(state, payload.todoItem);
-    default:
-      return state;
-  }
+ 
 };
