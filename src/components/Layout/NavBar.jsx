5afdd5291a26486c8adbb1349c3bb4dc4c9dcba6
Chukwujama Osinachi Victor <vicraph7@gmail.com>
2020-05-03 22:10:20 +0100

    Final Push for layout branch

--- src/components/Layout/NavBar.jsx
+++ src/components/Layout/NavBar.jsx
@@ -7,7 +7,7 @@ const NavBar = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
 
   return (
-    <Box top = "0" left = "0" paddingBottom = "10rem" zIndex = "10" position = "fixed" width = "100%" padding = "1rem" bg="gray.600" color="white">
+    <Box top = "0" left = "0" zIndex = "10" position = "fixed" width = "100%" padding = "1rem" bg="gray.600" color="white">
       <Flex align = "center">
         <Box onClick = {onOpen} as={FaBars}></Box>
         <Heading margin = "0 1rem" as = "h4" fontSize = "lg">FinRec</Heading>
